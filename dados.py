9be3a9092e39bfc3f5ea34a8c134b26d1de291e4
prc64 <manuel.isidrosanjuan@gmail.com>
2024-06-15 07:26:34 +0000

    Revert "Mensaje cambiado"
    This reverts commit 1e2da60f70b537c3d8eea52859a1bb6ded26c8b2.

--- dados.py
+++ dados.py
@@ -5,6 +5,6 @@ def roll(amount:int, sides:int):
 	return dice.roll(f'{amount}d{sides}')
 
 for idx, result in enumerate(roll(5, 6)):
-	print(f'Nuevo mensaje: Lanzamiento {idx+1} numero obtenido {result}')
+	print(f'Lanzamiento {idx+1} numero obtenido {result}')
 	sleep(5)
 
